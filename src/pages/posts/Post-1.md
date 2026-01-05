d4719d44544b2b5409a31a24b54929b1d8e1f8f5
lydia <lydiastew01@gmail.com>
2026-01-05 18:27:48 +0000

    sign up working!

---
+++ src/pages/posts/Post-1.md
@@ -0,0 +1,9 @@
+---
+layout: ../../layout/MarkDownPostLayout.astro
+title: "First blog post"
+author: Lydia
+description: "The first blog post"
+pubDate: 2026-01-05
+---
+
+# First Post!
--- src/supabase.js
+++ src/supabase.js
@@ -3,3 +3,4 @@ import { createClient } from '@supabase/supabase.js';
 const supabaseUrl = 'https://ximisbmpcyvjxdutrpjb.supabase.co';
 const supabaseKey = 'sb_publishable_5A-pOeeAQgMfRBwN35AfKw_QLxg6j4I';
 export const supabase = createClient(supabaseUrl, supabaseKey);
+
